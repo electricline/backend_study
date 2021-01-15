000a209b18ba73b0b26fa805212a39fe8419f6fb
electricline <jktmobile6637@gmail.com>
2021-01-15 23:08:19 +0900

    Initial commit

---
+++ src/test/java/com/harden/backend_study/BackendStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harden.backend_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
