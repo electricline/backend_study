0bc76d60b6fa3cf5473e98648043797b38c07bbe
electricline <jktmobile6637@gmail.com>
2021-01-29 00:46:38 +0900

    feature - 캐싱 초기화

--- src/main/java/com/harden/backend_study/domain/movie/service/MovieService.java
+++ src/main/java/com/harden/backend_study/domain/movie/service/MovieService.java
@@ -1,23 +1,33 @@
 package com.harden.backend_study.domain.movie.service;
 
+import com.harden.backend_study.cache.MovieLocalCacheImpl;
 import com.harden.backend_study.domain.movie.dto.Movie;
 import com.harden.backend_study.domain.movie.dto.MovieGroup;
 import com.harden.backend_study.domain.movie.repository.MovieRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class MovieService {
 
-
     private final MovieRepository movieRepository;
+    private final MovieLocalCacheImpl movieLocalCache;
 
-    public MovieService(MovieRepository movieRepository) {
+    public MovieService(MovieRepository movieRepository, MovieLocalCacheImpl movieLocalCache) {
         this.movieRepository = movieRepository;
+        this.movieLocalCache = movieLocalCache;
     }
 
     public List<Movie> search(final String query){
         return new MovieGroup(movieRepository.findByQuery(query)).getListOrderRating();
     }
+
+    public void initBookLocalCache(){
+        log.info("book local cache init");
+        movieLocalCache.clear();
+    }
+
 }
