12766af075cf41a45e2e949c45814663e79b823c
electricline <jktmobile6637@gmail.com>
2021-01-29 00:45:45 +0900

    update - 기존 검색 결과 캐싱 구현

--- src/main/java/com/harden/backend_study/domain/movie/repository/MovieRepositoryImpl.java
+++ src/main/java/com/harden/backend_study/domain/movie/repository/MovieRepositoryImpl.java
@@ -1,8 +1,11 @@
 package com.harden.backend_study.domain.movie.repository;
 
+
+import com.harden.backend_study.cache.MovieLocalCacheImpl;
 import com.harden.backend_study.domain.movie.dto.Movie;
 import com.harden.backend_study.domain.movie.dto.ResponseMovie;
 import com.harden.backend_study.config.NaverProperties;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -12,15 +15,18 @@ import org.springframework.web.client.RestTemplate;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Component
 public class MovieRepositoryImpl implements MovieRepository {
 
     private final RestTemplate restTemplate;
     private final NaverProperties naverProperties;
+    private final MovieLocalCacheImpl movieLocalCache;
 
-    public MovieRepositoryImpl(RestTemplate restTemplate, NaverProperties naverProperties) {
+    public MovieRepositoryImpl(RestTemplate restTemplate, NaverProperties naverProperties, MovieLocalCacheImpl movieLocalCache) {
         this.restTemplate = restTemplate;
         this.naverProperties = naverProperties;
+        this.movieLocalCache = movieLocalCache;
     }
 
     @Override
@@ -31,7 +37,12 @@ public class MovieRepositoryImpl implements MovieRepository {
         httpHeaders.add("X-Naver-Client-Secret", naverProperties.getClientSecret());
         String url = naverProperties.getMovieUrl() + "?query=" + query;
 
-        return restTemplate.exchange(url, HttpMethod.GET, new HttpEntity(httpHeaders), ResponseMovie.class)
+        if(movieLocalCache.containsKey(query)){
+            log.info("cache call");
+            return (List<Movie>) movieLocalCache.get(query);
+        }
+
+        List<Movie> movieList = restTemplate.exchange(url, HttpMethod.GET, new HttpEntity(httpHeaders), ResponseMovie.class)
                 .getBody()
                 .getItems()
                 .stream()
@@ -42,5 +53,10 @@ public class MovieRepositoryImpl implements MovieRepository {
                         .userRating(m.getUserRating())
                         .build())
                 .collect(Collectors.toList());
+
+        movieLocalCache.put(query, movieList);
+        log.info("cache miss");
+        return (List<Movie>) movieLocalCache.get(query);
+
     }
 }
