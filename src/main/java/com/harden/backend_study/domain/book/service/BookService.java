72a729defd37734df048c58664173d2a4b4d94c8
electricline <jktmobile6637@gmail.com>
2021-01-29 00:38:21 +0900

    feature - cache init service

--- src/main/java/com/harden/backend_study/domain/book/service/BookService.java
+++ src/main/java/com/harden/backend_study/domain/book/service/BookService.java
@@ -1,23 +1,32 @@
 package com.harden.backend_study.domain.book.service;
 
+import com.harden.backend_study.cache.BookLocalCacheImpl;
 import com.harden.backend_study.domain.book.dto.Book;
 import com.harden.backend_study.domain.book.repository.BookRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class BookService {
 
     private final BookRepository bookRepository;
+    private final BookLocalCacheImpl bookLocalCache;
 
-    public BookService(BookRepository bookRepository) {
+    public BookService(BookRepository bookRepository, BookLocalCacheImpl bookLocalCache) {
         this.bookRepository = bookRepository;
+        this.bookLocalCache = bookLocalCache;
     }
-
 
     public List<Book> search(final String query){
         return bookRepository.findByQuery(query);
+    }
+
+    public void initBookLocalCache(){
+        log.info("book local cache init");
+        bookLocalCache.clear();
     }
 
 }
