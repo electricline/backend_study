495611d58f25307a2f113111a0d2c3abdb5681a8
electricline <jktmobile6637@gmail.com>
2021-01-30 01:32:44 +0900

    exception - exception handler 생성

--- src/main/java/com/harden/backend_study/domain/movie/service/MovieService.java
+++ src/main/java/com/harden/backend_study/domain/movie/service/MovieService.java
@@ -4,6 +4,7 @@ import com.harden.backend_study.cache.MovieLocalCacheImpl;
 import com.harden.backend_study.domain.movie.dto.Movie;
 import com.harden.backend_study.domain.movie.dto.MovieGroup;
 import com.harden.backend_study.domain.movie.repository.MovieRepository;
+import com.harden.backend_study.global.error.exception.NaverApiRuntimeException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -30,7 +31,7 @@ public class MovieService {
         var query ="반지의 제왕";
 
         MovieGroup movieGroup = new MovieGroup(movieRepository.findByQuery(query));
-        return movieGroup.getHighestRatingMovie().orElse(Movie.builder().title("기본영화").link("http://").userRating(9.9f).build());
+        return movieGroup.getHighestRatingMovie().orElseThrow();
     }
 
     public void initMovieLocalCache(){
