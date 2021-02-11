1c5ceebecf5833240be50bdc45dfadda5f8c8c61
electricline <jktmobile6637@gmail.com>
2021-02-11 20:11:42 +0900

    feature - AOP annotation 구현

---
+++ src/main/java/com/harden/backend_study/config/aop/PerformanceTimeRecord.java
@@ -0,0 +1,13 @@
+package com.harden.backend_study.config.aop;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface PerformanceTimeRecord {
+
+}
